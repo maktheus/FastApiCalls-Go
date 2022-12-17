c02c48f4b8f7364bd05a8a80451fca919035aa9b
maktheus <maktheus@gmail.com>
2022-12-17 17:40:13 -0300

    readme

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 )
 
 var duration int
+var url := []string;
 
 
 func init() {
 
 	// Definimos a flag "duration" para o tempo de execução do script em segundos.
 	flag.IntVar(&duration, "d", 60, "tempo de execução do script em segundos")
+
+// Definimos a flag "urls" para as URLs da API.
+flag.StringArrayVar(&urls, "u", []string{"https://api.github.com/users/brunocarvalho"}, "URLs da API")
 
 	// Analisamos as flags.
 	flag.Parse()
